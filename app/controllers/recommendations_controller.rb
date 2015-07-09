0dcb6fa7294c7e1edf1fa3f9efc635af4f8ebc05
Duana Saskia <starkcoffee+apps@gmail.com>
2015-07-09 10:51:55 +0200

    Refactoring suggestions

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -3,37 +3,42 @@ class RecommendationsController < ApplicationController
 	def index
 	end
 
+  # def show
 	def suggestion
 		@name = sanitize(params[:name])
 		if @name.blank?
+       # TODO better way of handling error, eg. do validation of form in javascript
+       # controller could return an error status - how to do that in rails?
+       # render :index again, rather than redirecting root
 		   redirect_to root_path
 		else
-           find_user(@name) 
-		end
+      find_user(name)
+ 		end
 	end
-	 
+
 	private
 
-	def find_user(name)
-        user = User.find_by_name(name)
-        if user.nil?
-        	redirect_to root_path
-        else
-	        @products = user.products
-			@likes = likes_from(@products)
-			render :suggestion
-        end	
+  def find_user(name)
+    user = User.find_by_name(name)
+    if !user
+      # see above
+      redirect_to root_path
+    else
+      @products = user.products
+      @likes = likes_from(@products)
+      render :suggestion
     end
-	 
+  end
+
 	def sanitize(name)
 	    name.downcase.strip
 	end
-	 
+
 	def likes_from(products)
-	    likes = {} 
-		products.each do |product| 
-		  likes[product.product] = product.users 
-		end 
+	    likes = {}
+		products.each do |product|
+		  likes[product.product] = product.users
+		end
 		likes
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  # TODO rewrite this using concept of resources
   root 'recommendations#index'
+  # resource :recommendations
   get 'suggestion' => 'recommendations#suggestion'
 
   # You can have the root of your site routed with "root"
